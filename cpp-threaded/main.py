66ba2c533f086da1a34729900f2af300e8b14254
jorgenwh <jorgenwhen@gmail.com>
2022-08-13 04:20:38 +0200

    threaded

--- cpp-threaded/main.py
+++ cpp-threaded/main.py
@@ -3,31 +3,31 @@ import time
 from py_collatz import get_longest_sequence
 import build.foo as foo
 
-n = 500000
+n = 100000
 
 print(f"PY SOLUTION:")
-t1 = time.time_ns()
+t1 = time.time()
 py_seq = get_longest_sequence(n)
 py_len = len(py_seq)
-t2 = time.time_ns()
+t2 = time.time()
 elapsed = t2 - t1
-print(f"length={len(py_seq)}, elapsed_ns={elapsed}")
+print(f"length={len(py_seq)}, elapsed_secs={round(elapsed, 3)}")
 
 print(f"\nCPP SOLUTION:")
-t1 = time.time_ns()
+t1 = time.time()
 cpp_seq = foo.get_longest_sequence(n)
 cpp_len = len(cpp_seq)
-t2 = time.time_ns()
+t2 = time.time()
 elapsed = t2 - t1
-print(f"length={len(cpp_seq)}, elapsed_ns={elapsed}")
+print(f"length={len(cpp_seq)}, elapsed_secs={round(elapsed, 3)}")
 
 print(f"\nTHREADED CPP SOLUTION:")
-t1 = time.time_ns()
+t1 = time.time()
 threaded_cpp_seq = foo.get_longest_sequence_threaded(n, 12)
 threaded_cpp_len = len(threaded_cpp_seq)
-t2 = time.time_ns()
+t2 = time.time()
 elapsed = t2 - t1
-print(f"length={len(threaded_cpp_seq)}, elapsed_ns={elapsed}")
+print(f"length={len(threaded_cpp_seq)}, elapsed_secs={round(elapsed, 3)}")
 
 assert py_len == cpp_len == threaded_cpp_len
 for p, c, t in zip(py_seq, cpp_seq, threaded_cpp_seq):
