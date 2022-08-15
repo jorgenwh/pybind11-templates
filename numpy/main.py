27d49e005eb50ac2c794428750d259058db704ce
jorgenwh <jorgenwhen@gmail.com>
2022-08-15 02:27:23 +0200

    tuple

--- numpy/main.py
+++ numpy/main.py
@@ -5,26 +5,6 @@ print(dir(foo))
 
 import numpy as np
 
-arr = np.arange(10, dtype=np.float32)
-
-print(arr.dtype)
-print(arr)
-t1 = time.time_ns()
-arr += 1
-t2 = time.time_ns()
-elapsed = t2 - t1
-print(arr.dtype)
-print(arr)
-print(f"Elapsed: {elapsed} ns")
-
-arr = np.arange(10, dtype=np.float32)
-
-print(arr.dtype)
-print(arr)
-t1 = time.time_ns()
-arr = foo.foo_increment_arr(arr)
-t2 = time.time_ns()
-elapsed = t2 - t1
-print(arr.dtype)
-print(arr)
-print(f"Elapsed: {elapsed} ns")
+x = foo.get_np_array((2, 32))
+print(x.shape)
+print(x)
--- numpy/src/module.cpp
+++ numpy/src/module.cpp
@@ -1,4 +1,5 @@
 #include "foo_functions.h"
+#include <iostream>
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
@@ -13,4 +14,15 @@ PYBIND11_MODULE(foo, handle) {
 
   handle.def("foo_print_arr", &foo_print_arr);
   handle.def("foo_increment_arr", &foo_increment_arr);
+
+  handle.def("get_np_array", [](const py::tuple &t) {
+      std::vector<size_t> arr_shape(t.size());
+
+      for (size_t i = 0; i < t.size(); i++) {
+        arr_shape[i] = t[i].cast<size_t>(); 
+      } 
+
+      py::array_t<float> arr = py::array_t<float>(arr_shape);
+      return arr;
+  });
 }
